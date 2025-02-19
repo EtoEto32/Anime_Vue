d4464c0095180da57703f1753bbb9854a3adc4de
EtoEto32 <c1204041@st.kanazawa-it.ac.jp>
2025-02-19 23:50:12 +0900

    エフェクト変更＆ファビコン変更

--- vue-anime-app/src/components/HelloWorld.vue
+++ vue-anime-app/src/components/HelloWorld.vue
@@ -1,63 +1,34 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>
+      <span class="wavy-text">え</span>
+      <span class="wavy-text">っ</span>
+      <span class="wavy-text">と</span>
+      <span class="wavy-text">っ</span>
+      <span class="wavy-text">と</span>
+    </h1>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
-@Options({
-  props: {
-    msg: String
-  }
-})
-export default class HelloWorld extends Vue {
-  msg!: string
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+@keyframes wavy {
+  0% { transform: translateY(0px) rotate(0deg); }
+  25% { transform: translateY(-3px) rotate(3deg); }
+  50% { transform: translateY(2px) rotate(-2deg); }
+  75% { transform: translateY(-1px) rotate(1deg); }
+  100% { transform: translateY(0px) rotate(0deg); }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+
+.wavy-text {
   display: inline-block;
-  margin: 0 10px;
+  animation: wavy 1s infinite ease-in-out;
+  color: #50C878; /* エメラルドグリーン */
 }
-a {
-  color: #42b983;
-}
+
+/* ランダムなタイミングで開始させる */
+.wavy-text:nth-child(1) { animation-delay: 0s; }
+.wavy-text:nth-child(2) { animation-delay: 0.2s; }
+.wavy-text:nth-child(3) { animation-delay: 0.4s; }
+.wavy-text:nth-child(4) { animation-delay: 0.1s; }
+.wavy-text:nth-child(5) { animation-delay: 0.3s; }
 </style>
